2b40ee4b0bfd9f486155ecb5359d408c13c7dc12
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-08-17 19:41:42 +0200

    pages added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import SinglePost from "./Pages/SinglePost";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <Link to="/login">LOGIN</Link>
+
+      </header>
+
        <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/single-post" element={<SinglePost/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </div>
   );
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
 
 function About() {
   return (
-    <div>About</div>
+    <>
+    <main>
+      <h2>Who are we?</h2>
+      <p>
+        That feels like an existential question, don't you
+        think?
+      </p>
+    </main>
+    <nav>
+      <Link to="/">Home</Link>
+    </nav>
+  </>
   )
 }
 
