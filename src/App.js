dc95ef90ebc61d53a3b542f44ad66e8e7df2109f
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-08-17 19:01:44 +0200

    pages created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
 function App() {
   return (
     <div className="App">
-
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
