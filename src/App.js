676e33f71616cb4f1ae5b973e55b8826c5662ffc
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-08-17 19:57:45 +0200

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { Routes, Route, Link } from "react-router-dom";
-import './App.css';
+import './App.scss';
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import SinglePost from "./Pages/SinglePost";
 import Login from "./Pages/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header>
+        <logo>
+
+        </logo>
 
         <nav>
           <Link to="/">Home</Link>
           <Link to="/single-post">Post</Link>
-          <Link to="/login">LOGIN</Link>
+          <Link to="/login">Login</Link>
         </nav>
+        <div className="socials">
+
+        </div>
       </header>
 
        <Routes>
