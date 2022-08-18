cbfe8dae100fd51b9324a6f51c88ae6fe8f2d356
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-08-18 12:42:15 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import { Routes, Route, Link } from "react-router-dom";
-import './App.scss';
+import './App.css';
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import SinglePost from "./Pages/SinglePost";
 import Login from "./Pages/Login";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SocialFollow from "./Components/SocialFollow";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <logo>
-
+      <header className=" navbar navbar-expand-lg navbar-dark bg-dark">
+        <logo className="nav-items">
+          <img src={require('./logo.png')} className="logo" alt="logo" />
         </logo>
 
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/single-post">Post</Link>
-          <Link to="/login">Login</Link>
-        </nav>
-        <div className="socials">
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
 
+        <nav className="nav-items">
+          <Link to="/">Home</Link>
+
+          <DropdownButton
+            as={ButtonGroup}
+            key="myKey"
+            id="button"
+            title="👤"
+          >
+
+
+            <Dropdown.Item eventKey="1"> <Link to="/login">Login</Link></Dropdown.Item>
+            <Dropdown.Item eventKey="2"> <Link to="/profile">Profile</Link></Dropdown.Item>
+            <Dropdown.Item eventKey="3"> <Link to="/single-post">Post</Link></Dropdown.Item>
+
+          </DropdownButton>
+
+        </nav>
+
+
+        <div className="socials nav-items">
+          <SocialFollow />
         </div>
       </header>
 
