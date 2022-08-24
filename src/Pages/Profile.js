42df089dede9db3e6dffa0091e1e1d0a6a8151cc
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-08-24 18:20:03 +0200

    profile page created

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
 
 function Profile() {
+  const {id}=useParams();
+    const [profile, setProfile]=useState({})
+    useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(res=> res.json())
+        .then(data=>setProfile(data))
+    },[profile])
   return (
-    <div>Profile</div>
+    <div>
+         Profile
+        <h2>{profile.name}</h2>
+        <p>{profile.email}</p>
+    </div>
   )
 }
 
